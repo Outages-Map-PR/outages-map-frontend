1fcb947461937755007f53352aed8aa0c8bb3551
Alanis Y. Nazario <alanis.nazario@upr.edu>
2023-04-06 17:34:57 -0400

    +Big Nav Log in/out button

--- src/ComponentsCSS/Navbar.js
+++ src/ComponentsCSS/Navbar.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './/Css/Navbar.css';
 import {Link} from "react-router-dom";
-import {Button} from "./Button";
 import {
     HiMenuAlt2,
     HiOutlineFolderOpen,

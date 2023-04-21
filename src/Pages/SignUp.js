f1d0347c337620ffcfce29119151dcfaaefd9a9c
Hector Miranda <hector.miranda8@upr.edu>
2023-04-21 00:35:02 -0400

    Added Sign Up
    Sign Up pop up inside login page, upon success it shows a message to acknowledge and user is put on db, then can log in

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,99 +1,123 @@
+import { Box, Stack } from '@mui/material';
 import React, { useState } from 'react';
+import axios from 'axios';
+import { json } from 'react-router-dom';
 
-const SignUpForm = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
+const SignUpForm = ({close}) => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
+  const [successSignUp, setSuccessSignUp] = useState(false)
+
+  const dbUrl = 'http://127.0.0.1:5000/'
+  const userApi = dbUrl + 'API/user'
 
   const handleSignUp = () => {
-    // Perform form validation and submit data
-    // Here you can implement your own logic to validate form fields,
-    // handle form submission, and perform any necessary API requests
-    // to sign up the user.
-    // You can access the form field values from the state variables
-    // (e.g., username, email, phone, password, confirmPassword).
-    // You can also implement additional logic for formatting phone number,
-    // verifying email format, and checking password match, etc.
-    // This example just shows the basic structure of the form and its state.
-
-    // For example, you can implement password match validation as follows:
     if (password !== confirmPassword) {
-      setPasswordsMatch(false);
-      return;
+      setPasswordsMatch(false)
+      return
+    }
+    else {
+      setPasswordsMatch(true)
     }
 
-    // Perform form submission
-    // ...
+    let userData = JSON.stringify({
+      "user_email": email,
+      "user_password": password,
+      "user_name": username,
+      "user_type": "user",
+      "user_phone": phone
+    })
+
+    fetch(userApi, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: userData
+    })
+    .then(() => {
+      setSuccessSignUp(true)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
   };
 
+  const closeForm = () => {
+    close()
+  }
+
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Phone:
-          <input
-            type="text"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            maxLength={10}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            type="checkbox"
-            checked={showPassword}
-            onChange={() => setShowPassword(!showPassword)}
-          />
-          Show
-        </label>
-        <br />
-        <label>
-          Confirm Password:
-          <input
-            type={showPassword ? 'text' : 'password'}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </label>
-        {!passwordsMatch && (
-          <p style={{ color: 'red' }}>Passwords do not match</p>
-        )}
-        <br />
-        <button type="button" onClick={handleSignUp}>
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <Box sx={
+        {padding: '10px',
+        backgroundColor: 'whitesmoke',
+        maxWidth: '30vw'}}>
+      {successSignUp ? 
+        <>
+          <h3 style={{ color: 'green' }}>User successfully created.</h3>
+          <button onClick={closeForm}>Close</button>
+        </>
+       :
+        <Stack direction={'column'}>
+        <h2>Sign Up</h2>
+            <label>
+              Full Name:
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              Phone:
+              <input
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                maxLength={10}
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="checkbox"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+              />
+              Show
+            </label>
+            <label>
+              Confirm Password:
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </label>
+            {!passwordsMatch && (
+              <p style={{ color: 'red' }}>Passwords do not match</p>
+            )}
+          <button onClick={handleSignUp}>Sign Up</button>
+          <button onClick={closeForm}>Cancel</button>
+      </Stack>}
+      
+    </Box>
   );
 };
 

6643325632e032619f07d1009103c1a6cbe80621
Hector Miranda <hector.miranda8@upr.edu>
2023-05-01 15:31:04 -0400

    Add signup from main

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,128 +1,192 @@
-import { Box, Stack } from '@mui/material';
-import React, { useState } from 'react';
-import axios from 'axios';
-import { json } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import { json } from "react-router-dom";
+import { Check, CheckBox } from "@mui/icons-material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { IconBase } from "react-icons";
 
-const SignUpForm = ({close}) => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
-  const [passwordsMatch, setPasswordsMatch] = useState(true)
-  const [successSignUp, setSuccessSignUp] = useState(false)
-  const [user, setUser] = useState(0)
+const SignUpForm = ({ close }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [successSignUp, setSuccessSignUp] = useState(false);
 
-  const dbUrl = 'https://outages-db.herokuapp.com/'
-  const userApi = dbUrl + 'API/user'
+  const dbUrl = "https://outages-db.herokuapp.com/";
+  const userApi = dbUrl + "API/user";
 
   const handleSignUp = () => {
     if (password !== confirmPassword) {
-      setPasswordsMatch(false)
-      return
-    }
-    else {
-      setPasswordsMatch(true)
+      setPasswordsMatch(false);
+      return;
+    } else {
+      setPasswordsMatch(true);
     }
 
     let userData = JSON.stringify({
-      "user_email": email,
-      "user_password": password,
-      "user_name": username,
-      "user_type": "user",
-      "user_phone": phone
-    })
+      user_email: email,
+      user_password: password,
+      user_name: username,
+      user_type: "user",
+      user_phone: phone,
+    });
 
     fetch(userApi, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: userData
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: userData,
     })
-        .then((response) => {
-          setSuccessSignUp(true)
-          localStorage.setItem('user', response.data) //cookie
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+      .then(() => {
+        setSuccessSignUp(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   const closeForm = () => {
-    close()
-  }
+    close();
+  };
 
   return (
-      <Box sx={
-        {padding: '10px',
-          backgroundColor: 'whitesmoke',
-          maxWidth: '30vw'}}>
-        {successSignUp ?
-            <>
-              <h3 style={{ color: 'green' }}>User successfully created.</h3>
-              <button id="close" onClick={closeForm}>Close</button>
-            </>
-            :
-            <Stack direction={'column'}>
-              <h2>Sign Up</h2>
-              <label>
-                Full Name:
-                <input
-                    type="name"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-              </label>
-              <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-              </label>
-              <label>
-                Phone:
-                <input
-                    type="phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    maxLength={10}
-                />
-              </label>
-              <label>
-                Password:
-                <input
-                    id="password1"
-                    type={showPassword ? 'text' : 'password'}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <input
+    <Box
+      sx={{ padding: "10px", backgroundColor: "whitesmoke", width: "25vw" }}
+    >
+      {successSignUp ? (
+        <>
+          <h3 style={{ color: "green" }}>User successfully created.</h3>
+          <button onClick={closeForm}>Close</button>
+        </>
+      ) : (
+        <Stack direction={"column"} sx={{marginTop:"20px", marginLeft:"10px", marginRight:"10px", marginBottom:"20px"}}>
+          <h2 style={{ marginBottom: "5px" }}>Let's sign you up!</h2>
+          <h5 style={{ marginBottom: "20px", color: "grey" }}>
+            Welcome to the app
+          </h5>
 
-                    type="checkbox"
-                    checked={showPassword}
-                    onChange={() => setShowPassword(!showPassword)}
-                />
-                Show
-              </label>
-              <label>
-                Confirm Password:
-                <input
-                    id="password2"
-                    type={showPassword ? 'text' : 'password'}
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </label>
-              {!passwordsMatch && (
-                  <p style={{ color: 'red' }}>Passwords do not match</p>
-              )}
-              <button id="sign-up" onClick={handleSignUp}>Sign Up</button>
-              <button onClick={closeForm}>Cancel</button>
-            </Stack>}
+          <TextField
+            id="name"
+            type="text"
+            label="Full Name:"
+            value={username}
+            size="small"
+            onChange={(e) => setUsername(e.target.value)}
+            sx={{ marginBottom: "20px" }}
+          />
+          <TextField
+            id="email"
+            type="email"
+            label="Email:"
+            value={email}
+            size="small"
+            placeholder="example@mail.com"
+            onChange={(e) => setEmail(e.target.value)}
+            sx={{ marginBottom: "20px" }}
+          />
 
-      </Box>
+          <TextField
+            id="phone"
+            type="text"
+            label="Phone Number:"
+            value={phone}
+            size="small"
+            onChange={(e) => setPhone(e.target.value)}
+            sx={{ marginBottom: "20px" }}
+            maxLength={10}
+          />
+
+          <FormControl variant="outlined" sx={{marginBottom:"20px"}}>
+            <InputLabel htmlFor="password">Password:</InputLabel>
+            <OutlinedInput
+              id="password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                !showPassword ? (
+                  <InputAdornment>
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      <VisibilityOffIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ) : (
+                  <InputAdornment>
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      <VisibilityIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }
+              label="Password:"
+              value={password}
+              size="small"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl variant="outlined" sx={{marginBottom:"20px"}}>
+            <InputLabel htmlFor="confirmPassword">Confirm Password:</InputLabel>
+            <OutlinedInput
+              id="confirmPassword"
+              type={showConfirmPassword ? "text" : "password"}
+              endAdornment={
+                !showConfirmPassword ? (
+                  <InputAdornment>
+                    <IconButton
+                      onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                      edge="end"
+                    >
+                      <VisibilityOffIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ) : (
+                  <InputAdornment>
+                    <IconButton
+                      onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                      edge="end"
+                    >
+                      <VisibilityIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }
+              label="Confirm Password:"
+              value={confirmPassword}
+              size="small"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </FormControl>
+
+          {!passwordsMatch && (
+            <p style={{ color: "red" }}>Passwords do not match</p>
+          )}
+          <Button variant="contained" onClick={handleSignUp} sx={{backgroundColor: "#773deb", marginBottom:"10px"}}>Sign Up</Button>
+          <Button variant="outlined" onClick={closeForm}>Cancel</Button>
+        </Stack>
+      )}
+    </Box>
   );
 };
 
